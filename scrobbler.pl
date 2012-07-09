565217e9867e529a5156ff1d8bc870329d71dbe6
Roman (Ky6uk) Nuritdinov <ky6uk.kun@gmail.com>
2012-07-09 16:37:58 +0600

    update version

--- scrobbler.pl
+++ scrobbler.pl
@@ -1,30 +1,25 @@
 #!/usr/bin/env perl
 
-use EV;
-use 5.01;
 use strict;
 use warnings;
+
+use EV;
+use 5.01;
 use Getopt::Std;
 use Data::Dumper;
 use IO::Socket::INET;
-
-use Audio::Scrobbler qw(
-    auth_getToken
-    auth_getSession
-    track_updateNowPlaying
-    track_scrobble
-);
+use Audio::Scrobbler2;
 
 # user variables
 my $api_key    = "fc8ebcbc6bfec2cf047b3c163f5682eb";
 my $api_secret = "b57a5629f48d691ac178dc6b02ca58f5";
-my $api_session = "";
 my $mpd_host = "localhost";
 my $mpd_port = "6600";
 my $update_interval = 10;
 my $scrobble_interval = 90;
 
 # system variables
+my $scrobbler = Audio::Scrobbler2->new($api_key, $api_secret);
 my $counter = 0;
 my $scrobbled = 0;
 my $current_track = '';
@@ -36,12 +31,12 @@ getopts('rhs:', $options);
 
 if ( $options->{"s"} ) {
     ( $options->{"s"} =~ m/^[0-9a-f]{32}$/ ) ?
-        ( $api_session = $options->{"s"} ) :
+        ( $scrobbler->set_session_key($options->{"s"}) ) :
         ( die "invalid session string in -s" );
 }
 
-if ( $options->{"r"} and not $api_session ) {
-    my $api_token = auth_getToken($api_key);
+if ( $options->{"r"} ) {
+    my $api_token = $scrobbler->auth_getToken();
 
     if ($api_token and ref \$api_token eq "SCALAR") {
         say "auth url: http://www.last.fm/api/auth/?api_key=$api_key&token=$api_token" ;
@@ -50,7 +45,7 @@ if ( $options->{"r"} and not $api_session ) {
     }
     else { die "auth_getToken() exception", Dumper $api_token }
 
-    $api_session = auth_getSession($api_key, $api_secret, $api_token);
+    my $api_session = $scrobbler->auth_getSession();
 
     if ( $api_session and ref \$api_session eq "SCALAR" ) {
         say "Your API session key: $api_session";
@@ -128,7 +123,7 @@ my $watcher = EV::timer 0, $update_interval, sub {
     my ($artist, $track) = &get_track_info();
 
     if ($artist and $track) {
-        my $response = track_updateNowPlaying($artist, $track, $api_key, $api_secret, $api_session);
+        my $response = $scrobbler->track_updateNowPlaying($artist, $track);
 
         if ($response->{"error"}) {
             say "Update exception: " . $response->{"message"};
@@ -146,7 +141,7 @@ my $watcher = EV::timer 0, $update_interval, sub {
                 $current_track = "$artist - $track";
 
                 if ( not $scrobbled and $scrobble_interval / $update_interval <= $counter ) {
-                    my $response = track_scrobble($artist, $track, $api_key, $api_secret, $api_session);
+                    my $response = $scrobbler->track_scrobble($artist, $track);
 
                     if ($response->{"error"}) {
                         say "Scrobble exception: " . $response->{"message"};
@@ -159,6 +154,6 @@ my $watcher = EV::timer 0, $update_interval, sub {
             }
         }
     }
-} if $api_session;
+};
 
 EV::run;
